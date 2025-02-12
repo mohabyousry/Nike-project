51fd07f6dc23a9da53cb9a3e43181a18bbd52759
mohabyousry <mohabyousry@outlook.com>
2025-02-12 16:32:45 +0200

    adding new sections

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,18 @@ const App = () => {
       <section className="padding">
         <SuperQuality />
       </section>
-      <section className="padding bg-pale-blue<">
-        <CustomerReviews />
-      </section>
-      <section className="padding-x py-10<">
+      <section className="padding-x py-10">
         <Services />
       </section>
-      <section className="padding<"> Subscribe</section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding">
+        <Subscribe />
+      </section>
       <section className="bg-black padding-x padding-t pb-8">
         <Footer />
       </section>
